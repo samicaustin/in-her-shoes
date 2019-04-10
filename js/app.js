5347dcb40014ac762f40fe8ff696a37e24b1e366
Sami Austin <samicaustin@gmail.com>
2019-04-10 09:03:50 -0600

    Finalized content at last

--- js/app.js
+++ js/app.js
@@ -47,72 +47,41 @@ class Item {
             <button class="correct">${this.answer}</button>
             <button class="incorrect">${this.option2}</button>
              `);
-        } else if (this.number === 5) {
-            $(`.footer`).append(`
-            <button class="incorrect">${this.option1}</button>
-            <button class="incorrect">${this.option2}</button>
-            <button class="correct">${this.answer}</button>
-             `);
-        } else if (this.number === 6) {
-            $(`.footer`).append(`
-            <button class="correct">${this.answer}</button>
-            <button class="incorrect">${this.option1}</button>
-            <button class="incorrect">${this.option2}</button>
-             `);
-        } else if (this.number === 7) {
-            $(`.footer`).append(`
-            <button class="incorrect">${this.option1}</button>
-            <button class="correct">${this.answer}</button>
-            <button class="incorrect">${this.option2}</button>
-             `);
-        } else if (this.number === 8) {
-            $(`.footer`).append(`
-            <button class="incorrect">${this.option1}</button>
-            <button class="correct">${this.answer}</button>
-            <button class="incorrect">${this.option2}</button>
-             `);
-        } else if (this.number === 9) {
-            $(`.footer`).append(`
-            <button class="incorrect">${this.option1}</button>
-            <button class="incorrect">${this.option2}</button>
-            <button class="correct">${this.answer}</button>
-             `);
         } else {
             $(`.footer`).append(`
-            <button class="incorrect">${this.option2}</button>
             <button class="incorrect">${this.option1}</button>
+            <button class="incorrect">${this.option2}</button>
             <button class="correct">${this.answer}</button>
              `);
         } 
-        
     };
 
     loadQuestion = () => {
         $(`.main`).empty();
         $(`.main`).append(`<div class="question"> ${this.number}. ${this.question}</div>`);
         $(`.right`).empty();
-        $(`.right`).append(`<br> Question: <br> ${this.number} of 10`);
+        $(`.right`).append(`Scenario: <br> ${this.number} of 5`);
     };
 
     clickCorrect = () => {
         $('body').on(`click`, '.correct', () => {
         $(`.main`).empty();
-        $(`.main`).append(`<div class="answer">Correct. <br>${this.answer}</div><br>${this.explanation}`);
+        $(`.main`).append(`<div class="answer"><b>Correct.</b> <br>${this.answer}</div><br><br>${this.explanation}`);
         $(`.footer`).empty();
-        $(`.footer`).append(`<button class="next-button">Next Question</button>`);
+        $(`.footer`).append(`<button class="next-button">Next Scenario</button>`);
         $(`.topright`).empty();
-        $(`.topright`).append(`<br> Question: <br> ${this.number} of 10`);
+        $(`.topright`).append(`Scenario: <br> ${this.number} of 5`);
     });
     };
 
     clickIncorrect = () => {
         $('body').on(`click`, '.incorrect', () => {
         $(`.main`).empty();
-        $(`.main`).append(`<div class = "explanation">Not quite. <br>${this.explanation}</div>`);
+        $(`.main`).append(`<div class = "explanation"><b>Not quite.</b> <br><br>${this.explanation}</div>`);
         $(`.footer`).empty();
         $(`.footer`).append(`<button class="next-button">Next Question</button>`);
         $(`.topright`).empty();
-        $(`.topright`).append(`<br> Question: <br> ${this.number} of 10`);
+        $(`.topright`).append(`Scenario: <br> ${this.number} of 5`);
     });
     };
 
@@ -151,46 +120,6 @@ class Item {
             }
         )}  else if (this.next === 6) {
             $(`body`).on('click', `.next-button`, () => {
-                six.loadOptions();
-                six.loadQuestion();
-                six.clickCorrect();
-                six.clickIncorrect();
-                six.loadNext();
-                }
-        )} else if (this.next === 7) {
-            $(`body`).on('click', `.next-button`, () => {
-                seven.loadOptions();
-                seven.loadQuestion();
-                seven.clickCorrect();
-                seven.clickIncorrect();
-                seven.loadNext();
-                }
-        )}  else if (this.next === 8) {
-            $(`body`).on('click', `.next-button`, () => {
-                eight.loadOptions();
-                eight.loadQuestion();
-                eight.clickCorrect();
-                eight.clickIncorrect();
-                eight.loadNext();
-                }
-        )}else if (this.next === 9) {
-            $(`body`).on('click', `.next-button`, () => {
-                nine.loadOptions();
-                nine.loadQuestion();
-                nine.clickCorrect();
-                nine.clickIncorrect();
-                nine.loadNext();
-                }
-        )} else if (this.next === 10) {
-            $(`body`).on('click', `.next-button`, () => {
-                ten.loadOptions();
-                ten.loadQuestion();
-                ten.clickCorrect();
-                ten.clickIncorrect();
-                ten.loadNext();
-                }
-        )} else {
-            $(`body`).on('click', `.next-button`, () => {
             loadResults();
             }
         )}; 
@@ -202,40 +131,44 @@ $('body').on(`click`, '.correct', () => {
 addPoint();
 });
 
+
 // OBJECT INSTANCES and ITEMS ARRAY TO LOOP THROUGH!
+const one = new Item(1, 2, "'J' is a Black woman who has been working as a highly-successful mid-level administrator at her company for four years. She works with a few other women, but at any given point in her day, she is the only person of color in the room. <br><br>She woke up this morning feeling extremely sick, but has an important presentation today. What will J do?", 
+"She will do her presentation and work even harder to compensate.", 
+"She will stay home and take care of herself--the presentation can be rescheduled!", 
+"She will go to work but take it easy so that she can recover more quickly.", 
+"'J', like many people who are the only person of a given race, gender, or sexuality at their workplace, feels extra pressure to preform. She says, 'I need to come across as more than proficient, more than competent, more than capable. <br><b>I have to be on all the time.</b>'");
 
-const one = new Item(1, 2, "J is a Black woman who has been working as a highly-successful mid-level administrator at her company for four years. She works with a few other women, but at any given point in her day, she is the only person of color in the room. She woke up this morning feeling extremely sick, but has an important presentation today. What will J do?", 
-"She will do her presentation and accomplish more than usual.", 
-"She should stay home and take care of herself--the presentation can be rescheduled!", 
-"She should go to work but take it easy so that she can recover more quickly.", 
-"J, like many people who are the only person of a given race, gender, or sexuality at their workplace, feels extra pressure to preform. She says, 'I need to come across as more than proficient, more than competent, more than capable. I have to be on all the time.'");
+const two = new Item(2, 3, "‘L’ is a white lesbian who is the senior vice president at the company she’s served for over ten years. <br> <br> On any given day, how much more likely is it that she’ll hear someone say something demeaning about her or people like her than it is for a man?", 
+"2.5 times more likely.", "2 times more likely.", "1.5 times more likely.", 
+"'L' and other lesbians are 250% more likely than straight men to hear discriminatory remarks about themselves or about the queer community. Over the course of her career, 'L' has had supervisors who have, intentionally or not, made her feel as if it were inappropriate to mention her wife.");
 
-const two = new Item(2, 3, "What is your favorite sport?", "soccer", "option1", "option2", "option3", "article");
-const three = new Item(3, 4, "What is your favorite hobby?", "drawing", "option1", "option2", "option3", "comic");
-const four = new Item(4, 5, "What is your favorite movie?", "movie", "option1", "option2", "option3", "poster");
-const five = new Item(5, 6, "What is your favorite Spice Girl?", "Scary", "option1", "option2", "option3", "image");
-const six = new Item(6, 7, "Another question", "another answer", "option1", "option2", "option3", "another resource");
-const seven = new Item(7, 8, "question seven", "answer seven", "option1", "option2", "option3", "comic");
-const eight = new Item(8, 9, "eighth question", "eighth answer", "option1", "option2", "option3", "eighth resource");
-const nine = new Item(9, 10, "ninth question", "ninth answer", "option1", "option2", "option3", "article");
-const ten = new Item(10, 11, "tenth and final question", "tenth and final answer", "option1", "option2", "option3", "resource");
+const three = new Item(3, 4, "'M', a Director at her company, is an Asian woman who is subject to microagressions, or acts of daily discrimination, regularly. <br><br>Recently in the elevator, she experienced something 22% of Asian women face at some point in their careers. What was it?", 
+"She was mistaken for someone in a much more junior position.", "She was ignored entirely by the people inside.", "She was directly called a racial slur", "'M' recalls the incident: 'I was in the elevator and pressed the button for the executive office. Someone said to me ‘Um, no honey. That’s for the executive offices. The interns are going to this floor.’<br> <br>Incidents like these can make women feel undervalued and disrespected.");
+
+const four = new Item(4, 5, "'S' is a Middle Eastern woman who works as an associate at her predominantly male organization.<br><br>Although she gets along with her male peers, she often feels left out socially. What percentage of employees who are the only women at their companies feel like 'S' does?", 
+"75% of women.", "50% of women.", "25% of women.", "Only one in four women feel included socially in their workplace when they are the only woman present. 'S' describes her workplace as 'a boys' club.' Relationships with peers and supervisors are a critical piece of professional success, and this exclusion is yet another set back for women.");
+
+const five = new Item(5, 6, "'T' is a Senior Executive at her company, and a Latina woman. <br><br>There is an enormous pressure placed on her everyday that is the most common microagression experienced in the workplace. <br><br> What is she experiencing?", 
+"Having to prove evidence of her competence more than others.", "Having to look a certain way in order to be accepted.", "Having to ask for support more often than others.", 
+"While reflecting on her career, 'T' says:<br> 'One thing I’ve become used to is having to prove myself constantly, over and over. It’s tiring, and unfortunately it hasn’t changed a whole lot as I’ve become more senior.”<br><br> A third of all professional women find themselves having to provide much more evidence of their competence than their male peers.");
+
 
 // LANDING PAGE
 $(`.header`).append(`<header>IN HER SHOES</header>`);
-$('.main').append(`<div class="landing"> <br> 
-<h1>A working woman's life isn't easy. <br> 
-Do you have what it takes to walk in her shoes?</h1>
+$('.main').append(`<div class="landing">
+<i>A professional woman faces challenges that can make it <br> hard to be happy, successful, and safe at work. </i><br> 
+<br><h1> Do you have what it takes to walk a mile in her shoes?</h1>
 </div>`);
 
 $(`.right`).append(`
-    <b>Source Info</b><br>
-    All content from In Her Shoes is based off of data from <a href="https://www.mckinsey.com/featured-insights/gender-equality/women-in-the-workplace-2018">Women in the Workplace 2018</a>:
+    <b>SOURCE INFO</b><br>
+    All In Her Shoes data and direct quotes are from real women via the <a href="https://www.mckinsey.com/featured-insights/gender-equality/women-in-the-workplace-2018">Women in the Workplace 2018</a> report:
     the largest comprehensive study of the state of women in corporate America. <br>
     `);
-    $(`.left`).append(`<b>Gameplay</b><br>
+    $(`.left`).append(`<b>GAMEPLAY</b><br>
       
-    Gain points by anticipating choices that you think will improve or maintain each woman's safety, 
-    security, and success at work by condidering the following scenarios carefully. 
+    Earn points by anticipating others' actions, tapping into others' perspectives, and answering questions about diversity and inclusion in the workplace.
     `);
 
 $(`.footer`).append(`<input type="text" name="name" class="name-input" placeholder="What's your name?"/>`);
@@ -252,10 +185,9 @@ const loadIntro = (name) => {
     $(`.left`).empty();
     $(`.right`).empty();
     $(`.landing`).replaceWith(`<div class="intro">
-       <i>In today's professional world, women continue to be vastly underrepresented
-       and mistreated at every level. <br><br> For women of color, it’s even worse. <br> <br>
-        Progress isn’t just slow-—it’s stalled. </i><br><br>
-       <h1> Take a walk in her shoes, ${name}, to find out why.</h1> <br>
+       <i>In today's professional world, women continue to be vastly underrepresented at every level. 
+       They compose only 38% of managers and a mere 22% of C-Suite positions.</i><br><br>
+       <h1> Click "Begin", ${name}, to learn about some of the challenges women face in the workplace.</h1> <br>
       
     </div>`);
     $(`.footer`).append(`<button class="begin-button">Begin</button>`);
@@ -268,7 +200,7 @@ const loadIntro = (name) => {
 const loadFirst = () => {
     $(`body`).on('click', `.begin-button`, () => {
         $(`.left`).append(`Points: <div class="points">${points}</div>`);
-        $(`.right`).append(`Question: <br> 1 of 10`);
+        $(`.right`).append(`Scenario: <br> 1 of 5`);
         one.loadOptions();
         one.loadQuestion();
         one.clickCorrect();
@@ -280,8 +212,15 @@ const loadFirst = () => {
 
 // RESULTS
 const loadResults = () => {
+    $(`.left`).empty();
+    $(`.right`).empty();
     $(`.main`).empty();
-    $(`.main`).text(`You earned a total of ${points} points out of a possible 10.`)
+    $(`.main`).append(`<h1>You earned a total of ${points} points out of a possible 5, ${name},
+    but your work to fight gender and racial inequity in today's workplace has only just begun. </h1><br>
+    Check out the 
+    <a href="https://www.mckinsey.com/featured-insights/gender-equality/women-in-the-workplace-2018">
+    Women in the Workplace 2018</a> report <br>and continue educating yourself, your peers, 
+    and your organization <br>on what we as professionals can do to make serious changes happen.`)
     $(`.footer`).empty();
     $(`.footer`).append(`<button class = "try-again">Try Again?</button>`);
     $(`.footer`).on(`click`, `.try-again`, () => {
